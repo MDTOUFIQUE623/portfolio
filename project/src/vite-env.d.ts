d26c09de5679bd9fef96791419bf6befa72322ca
MDTOUFIQUE623 <ahmedtoufiqkhan37@gmail.com>
2024-11-26 11:38:14 +0530

    code is good

--- project/src/vite-env.d.ts
+++ project/src/vite-env.d.ts
@@ -1,9 +1,1 @@
 /// <reference types="vite/client" />
-import React from 'react'
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      [elemName: string]: any;
-    }
-  }
-}
