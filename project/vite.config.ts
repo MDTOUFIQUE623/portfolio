fd1094acfb24d00c36b1ad58b67503b10e152cca
MDTOUFIQUE623 <ahmedtoufiqkhan37@gmail.com>
2024-11-26 10:26:35 +0530

    again

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,17 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist',
+    sourcemap: true,
     rollupOptions: {
-      input: path.resolve(__dirname, 'index.html'),
-    },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          utilities: ['framer-motion', 'marked']
+        }
+      }
+    }
+  }
 })
